27afaf051f1618d045b60f2bcd1d9121a53c1da8
FrancoAdrianCarrizo <franco.carrizo@bitlogic.io>
2021-07-06 18:55:17 -0300

    First commit

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,63 +1,64 @@
-import React, { useMemo } from 'react'
-import { Redirect, useParams } from 'react-router-dom'
-import { getHeroById } from 'selectors/getHeroById'
+import React, { useMemo } from 'react';
+import { useParams, Redirect } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
 
-const heroImages = require.context('../../assets/heroes')
+export const HeroScreen = ({ history }) => {
 
-export const HeroScreen = ({history}) => {
+    const { heroeId } = useParams();
 
-    // @ts-ignore
-    const { heroId } = useParams()
+    const hero = useMemo(() => getHeroById( heroeId ), [ heroeId ]);
 
-    // const hero = getHeroById(heroId)
 
-    const hero = useMemo(() =>  getHeroById(heroId), [heroId])
-
-    if (!hero) {
-        return <Redirect to="/" />
+    if ( !hero ) {
+        return <Redirect to="/" />;
     }
 
     const handleReturn = () => {
 
-        if(history.length <= 2){
-            history.push('/')
-        }else{
+        if( history.length <=2 ) {
+            history.push('/');
+        } else {
             history.goBack();
         }
+
     }
 
-    const { id, superhero, publisher, alter_ego, first_appearance, characters } = hero
-
-
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+    } = hero;
+    
     return (
         <div className="row mt-5">
             <div className="col-4">
-                <img src={heroImages(`./${id}.jpg`).default} alt={hero.superhero}
+                <img 
+                    src={ `../assets/heroes/${ heroeId }.jpg` }
+                    alt={ superhero }
                     className="img-thumbnail animate__animated animate__fadeInLeft"
                 />
             </div>
-            <div className="col-8">
-                <h3>{superhero}</h3>
+
+            <div className="col-8 animate__animated animate__fadeIn">
+                <h3> { superhero } </h3>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item">
-                        <b>Alter ego: </b>
-                        {alter_ego}
-                    </li>
-                    <li className="list-group-item">
-                        <b>Publisher: </b>
-                        {publisher}
-                    </li>
-                    <li className="list-group-item">
-                        <b>First Appearance: </b>
-                        {first_appearance}
-                    </li>
+                    <li className="list-group-item"> <b> Alter ego: </b> { alter_ego } </li>
+                    <li className="list-group-item"> <b> Publisher: </b> { publisher } </li>
+                    <li className="list-group-item"> <b> First appearance: </b> { first_appearance } </li>
                 </ul>
-                <h5>Charactes</h5>
-                <p>{characters}</p>
+
+                <h5> Characters </h5>
+                <p> { characters } </p>
+
                 <button 
-                    onClick={handleReturn}
                     className="btn btn-outline-info"
-                >Return</button>
+                    onClick={ handleReturn }
+                >
+                    Return
+                </button>
+
             </div>
 
         </div>
